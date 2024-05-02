df36e7904d9204b78c34bcf90bcad671213cf781
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-05-02 17:38:51 +0200

    m-1  created bomb and added inside square with random number

--- js/main.js
+++ js/main.js
@@ -2,6 +2,12 @@
 let grid = document.getElementById("grid")
 let start = document.getElementById("start")
 let select = document.getElementById("difficulty")
+//creo variabile bomb per le bombe che creerò con generatori di numeri random
+let bomb = ""
+//creo variabile contatore per ciclo while
+let i = 1
+//creo variabile dove pusho la lista di numeri che devo associare alle bombe
+let arrayBomb = []
 
 //inizio funzione che mi crea il quadrato
 function createSquare(qualcosa) {
@@ -9,6 +15,7 @@ function createSquare(qualcosa) {
     let quadrato = document.createElement("div")
     //aggiungo una class al div con variabile quadrato
     quadrato.classList.add("square")
+    //aggiungo a square la classe per icona bombetta in ogni square
     quadrato.classList.add("fa-solid")
     quadrato.classList.add("fa-bomb")
 
@@ -33,6 +40,20 @@ function createSquare(qualcosa) {
 start.addEventListener("click", function () {
     grid.innerHTML = ""
     if (select.value == "easy") {
+        //Inserisco il ciclo while che mi crea 16 bombette random senza creare doppioni e finche la lista non ha 16 numeri perchè come condizione ho messo lunghezza della lista
+        while (arrayBomb.length <=15){
+            bomb = Math.floor(Math.random() * 100) + 1;
+            //se la lista include già quel numero allora non lo pusha
+            if(arrayBomb.includes(bomb)){
+            
+            }
+            //sennò se nella lista quel numero non ce pusha il numero nella lista 
+            else{
+                console.log("Le bombe sono la numero:" ,bomb)
+                arrayBomb.push(bomb)
+            }
+            i++
+        }
         console.log(select.value)
         for (let i = 1; i <= 100; i++) {
             //Console log per stampare i numeri da 1 a 100 in console
@@ -44,6 +65,15 @@ start.addEventListener("click", function () {
             grid.append(square)
         }
     }else if (select.value == "medium") {
+        while (arrayBomb.length <=15){
+            bomb = Math.floor(Math.random() * 81) + 1;
+            if(arrayBomb.includes(bomb)){
+            }else{
+                console.log("Le bombe sono la numero:" ,bomb)
+                arrayBomb.push(bomb)
+            }
+            i++
+        }
         console.log(select.value)
         for (let i = 1; i <= 81; i++) {
             //Console log per stampare i numeri da 1 a 100 in console
@@ -54,7 +84,16 @@ start.addEventListener("click", function () {
             //appendo alla griglia in html square
             grid.append(square)
         }
-    }else if (select.value == "hard") { 
+    }else if (select.value == "hard") {
+        while (arrayBomb.length <=15){
+            bomb = Math.floor(Math.random() * 49) + 1;
+            if(arrayBomb.includes(bomb)){
+            }else{
+                console.log("Le bombe sono la numero:" ,bomb)
+                arrayBomb.push(bomb)
+            }
+            i++
+        }
         console.log(select.value)
         for (let i = 1; i <= 49; i++) {
             //Console log per stampare i numeri da 1 a 100 in console
